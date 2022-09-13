7b41b73a02b6c913d9453e817317f248a43a4c7c
camilo3364 <camijuan_321@hotmail.com>
2022-09-13 10:29:15 -0500

    Tratamiento de errores y repetir juego

--- game.py
+++ game.py
@@ -1,4 +1,3 @@
-from pickle import FALSE
 import random as rnd
 import sys
 
@@ -6,14 +5,11 @@ import sys
 def wordReplace(entryWord):
     word = entryWord
     otherWord = otherWord = "".join(word)
-    #print("Esta es la palabra:"+ str(word))
     newWord = (("a","á"),("e","é"),("i","í"),("o","ó"),("u","ú"))
     for i,j in newWord:
         for value in word:
             if value == j:
-                #print("Esta es la unión"+str(otherWord),value,i)
                 otherWord = otherWord.replace(value,i)           
-    #print("Esta es la palabra final " +str(otherWord))
     return otherWord
 
 
@@ -43,50 +39,44 @@ def listWord():
     return list
 
 
-
-
 def run():
-    guess = []
-    guess = wordReplace(listWord())
-    finalList = []
-    guessWord = FALSE
-    life = True
-    print("arrancó" +str(guess))
+    try:
+        guess = []
+        guess = wordReplace(listWord())
+        finalList = []
+        guessWord = True
+        life = True
+        word = ""
+        print("arrancó" +str(guess))
 
-    for i in guess:
-        finalList.append("_")
-    print(finalList)
-
-    while guessWord and life:  
-        #Creación de los espacios vacios
-        
-        
-        entryWord = [(input("Ingrese una letra: ")).lower()]
-        newWord = wordReplace(entryWord)
-        counter = 0 
         for i in guess:
-            counter +=1
-            if i == newWord:
-                finalList[counter-1] = i
+            finalList.append("_")
         print(finalList)
-        word ="".join(finalList) 
 
-        if guess == word:
-            guessWord = True
-            print("¡Adivinaste la palabra!")
-            answer = (input("¿Quieres jugar de nuevo?" + "\nYes o No:"+ "\n")).lower
-            if answer == "yes":
-                run()
-            else :
-                print("JUEGO TERMINADO")
-                sys.exit()
-        
-        
-    
-    #openTxt()
-    #random_word()
-    #print(listWord())
-    
+        while guessWord:  
+            entryWord = [(input("Ingrese una letra: ")).lower()]
+            newWord = wordReplace(entryWord)
+            counter = 0 
+            for i in guess:
+                counter +=1
+                if i == newWord:
+                    finalList[counter-1] = i
+            print(finalList)
+            word ="".join(finalList) 
+            if guess == word:
+                guessWord = False
+                print("¡Adivinaste la palabra!")
+        answer = (input("¿Quieres jugar de nuevo?" + "\nYes o No:"+ "\n")).lower()
+        print(answer)
+        if answer == "yes":
+            run()
+            print("ingresó al if")
+        else :
+            print("JUEGO TERMINADO")
+            sys.exit()
+    except ValueError:
+        print("Ingrese solo letras")
+
 
 if __name__=="__main__":
     run()
